649779d09d9131956356057a10bc5250dac74fc8
Hrishikesh Sathyian <e1121418@u.nus.edu>
2025-11-05 14:48:01 +0100

    Remove paywalled articles from scraping

--- scripts/fetch_article.py
+++ scripts/fetch_article.py
@@ -1,16 +1,21 @@
 import feedparser, requests
 from bs4 import BeautifulSoup
 from newspaper import Article
+
+
 # # RSS validity check
 # feed = feedparser.parse("https://www.straitstimes.com/news/business/rss.xml")
 
 # for entry in feed.entries: 
 #     print(entry.title)
-import nltk
-nltk.download('punkt_tab')
 
-business_articles = []
-count = 0
+## last modified stored as : 2025-10-08T15:59:07.000Z
+
+non_paywall_business_articles = []
+total_count = 0
+paywalled_articles = 0
+stop_limit = 10 # js to make testing faster
+
 for month in range(10, 0, -1): 
     url = "https://www.straitstimes.com/sitemap/2025/{}/feeds.xml".format(month)
     soup = BeautifulSoup(requests.get(url).content, "xml")
@@ -18,31 +23,22 @@ for month in range(10, 0, -1):
         if "/business/" in loc.text:
             last_modified = loc.find_next("lastmod")
             if not last_modified: 
-                last_modified = "N/A"
                 print("No last modified date found for article: ", loc.text)
-                break
+                continue
             else: 
                 last_modified = last_modified.text
-            business_articles.append((loc.text, last_modified))
-            count += 1
-            print("Fetched {} business articles".format(count))
+            total_count += 1
+            article = Article(loc.text)
+            article.download() 
+            if "For subscribers" in article.html: 
+                paywalled_articles += 1
+                continue
+            non_paywall_business_articles.append((loc.text, last_modified))
+            print("Fetching article: ", loc.text)
+            if total_count >= stop_limit:
+                break
 
-article = Article(business_articles[0][0])
+print("Total non-paywalled business articles fetched: ", len(non_paywall_business_articles))
+print("Total paywalled articles skipped: ", paywalled_articles)
 
-from sumy.parsers.plaintext import PlaintextParser
-from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lsa import LsaSummarizer
 
-article.download() 
-article.parse()
-article.nlp()
-# Create parser and summarizer
-parser = PlaintextParser.from_string(article.text, Tokenizer("english"))
-summarizer = LsaSummarizer()
-
-# Generate summary (3 sentences)
-summary = summarizer(parser.document, 3)
-summary_text = ' '.join([str(sentence) for sentence in summary])
-dummy_summary = article.summary
-print("Better Summary:", summary_text)
-print("Newspaper3k Summary:", dummy_summary)
